73bce7b853607e405d5cfe17855f85a2d5a21ef9
Park <mayer0425@naver.com>
2020-11-06 02:29:11 +0900

    201106

---
+++ Assets/Scripts/jibeom/Button_Exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Exit : MonoBehaviour
+{
+    public void ExitStage()
+    {
+        SceneManager.LoadScene("stage");
+    }
+}
